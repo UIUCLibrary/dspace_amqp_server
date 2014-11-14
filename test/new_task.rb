2f09a05e7a5f4913ffcb59e45eed432daeb913c6
Bill Ingram <waingram@gmail.com>
2014-11-14 16:04:58 -0600

    update test to point to proper bag

--- test/new_task.rb
+++ test/new_task.rb
@@ -10,9 +10,9 @@ conn.start
 ch   = conn.create_channel
 q    = ch.queue("va_to_ideals", :durable => true)
 
-bag_url = "https://github.com/Data-to-Insight-Center/sead-virtual-archive/raw/d2e71a78ab7fd535223bbf679f567cd1b00a5fb1/SEAD-VA-extensions/services/bagItRestService/src/test/resources/org/seadva/bagit/sample_bag.zip"
+bag_url = "https://raw.githubusercontent.com/UIUCLibrary/dspace_amqp_server/master/test/Vortex2Visualization.zip"
 
-msg  = {"action" => "ideals_deposit", "parameters" => {"message_name" => "bag-stage-location", "correlation_id" => "ea7bd000-df4f-11e3-8b68-0800200c9a66", "protocol" => "http", "location" => "#{bag_url}"}}.to_json
+msg  = {"action" => "ideals_deposit", "parameters" => {"message_name" => "bag-stage-location", "correlation_id" => "d6d250ba-e54d-4ae0-937d-c23d5e8b5de8", "protocol" => "http", "location" => "#{bag_url}"}}.to_json
 
 q.publish(msg, :persistent => true)
 puts " [x] Sent #{msg}"
